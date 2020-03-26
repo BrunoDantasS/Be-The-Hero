19be0348bb620665b3cf14384b166d45b24bc6ea
Bruno Wesley <bruno.wesley77@gmail.com>
2020-03-26 18:28:11 -0300

    add front-end

---
+++ front-end/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import './global.css';
+
+import Routes from './routes';
+
+/**
+ * O componente no REACT é uma função que retorna HTML,CSS, etc.
+ * Os componentes começam com letra maiúscula
+ */
+function App() {
+  return (
+    <Routes />
+  );
+}
+
+export default App;
