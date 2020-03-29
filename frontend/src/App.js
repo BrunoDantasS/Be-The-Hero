a48c299293d5e0c4fb7e87c6e76b838b10b7a147
Bruno Wesley <bruno.wesley77@gmail.com>
2020-03-28 22:23:45 -0300

    add mobile

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,10 +4,6 @@ import './global.css';
 
 import Routes from './routes';
 
-/**
- * O componente no REACT é uma função que retorna HTML,CSS, etc.
- * Os componentes começam com letra maiúscula
- */
 function App() {
   return (
     <Routes />
